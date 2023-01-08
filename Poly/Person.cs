e8813eab5ebe387a13980ff9e4f04fcc6f1b58e4
mmf09 <mmf09@YPC>
2023-01-07 16:09:09 -0800

    Add project files.

---
+++ Poly/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public abstract class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public abstract void SayName();
+    }
+
+    public class Employees : Person, IQuittable
+    {
+
+        public void Quit()
+        {
+            Console.WriteLine("Goodbye");
+        }
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+}
